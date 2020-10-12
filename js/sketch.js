d0cb35f1ac85e41e7e4b3129f6d3f0aabbb859d9
emzduck11 <emzduck11@gmail.com>
2020-10-11 22:09:33 -0400

    added styles

--- js/sketch.js
+++ js/sketch.js
@@ -11,14 +11,20 @@ rectMode(CENTER);
 
 function draw() {
   fill(120);
+  stroke(40,200,100);
+  strokeweight(10);
   point(500,300);
+  nostroke()
   // put drawing code here to run in loop
   ellipse(300,500,150,150);
 
   fill(50,200,120);
   rect(800,300,200,300);
 
-  fill(50,30,120);
+
+  stroke(40,200,100);
+  strokeweight(20);
+   fill(50,30,120);
   triangle(1200,50,1300,600,950,400);
 
 
