2b8960d5be5678941295b9ed1bd3ab625584efdb
emzduck11 <emzduck11@gmail.com>
2020-10-11 22:22:19 -0400

    added x and y

--- js/sketch.js
+++ js/sketch.js
@@ -11,22 +11,33 @@ rectMode(CENTER);
 
 function draw() {
   smooth();
+
+  //style for point
   fill(120);
   stroke(40,200,100);
   strokeWeight(10);
-  point(500,300);
-  nostroke()
+  point(mouseX,mouseY);
+
+
+  //style for ellipse
+  noStroke();
   // put drawing code here to run in loop
   ellipse(300,500,150,150);
 
+
+  //style for rectangle
+stroke(100,255,80);
+strokeWeight(20);
   fill(50,200,120);
   rect(800,300,200,300);
 
-
+//style for triangle
   stroke(40,200,100);
   strokeWeight(20);
    fill(50,30,120);
   triangle(1200,50,1300,600,950,400);
+
+  print(mouse);
 
 
 }
