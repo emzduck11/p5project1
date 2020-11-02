a7c5e8231f0e6d8f7a903ca10ade46b10a6dcbd5
emzduck11 <emzduck11@gmail.com>
2020-11-01 23:56:52 -0500

    added images

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,15 @@
 
+let cart;
+let sourworms;
+let strawberries;
+
+
+function preload(){
+  cart= loadImage("images/cart.png");
+  sourworms= loadImage("images/sourworms.png");
+  strawberries= loadImage("images/strawberries.png");
+}
+
 function setup() {
 
   createCanvas(500,500);
