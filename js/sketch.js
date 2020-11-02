b190eec016021db80c517cdcb56a8c29f35b5bcd
emzduck11 <emzduck11@gmail.com>
2020-11-02 06:13:30 -0500

    sd

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,11 @@
 
-let cart1;
+//let cart1;
 //let sourworms;
 //let strawberries;
 
 
-function preload(){
-  cart1= loadImage("images/cart1.png");
+//function preload(){
+//  cart1= loadImage("images/cart1.png");
 //  sourworms= loadImage("images/sourworms.png");
   //strawberries= loadImage("images/strawberries.png");
 }
@@ -13,7 +13,7 @@ function preload(){
 function setup() {
 
   createCanvas(500,500);
-  background(0);
+  background(300,50,120);
 
 
 }
