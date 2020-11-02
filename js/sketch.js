351d357242fb5bc48bf640c626a73f8c0c52757a
emzduck11 <emzduck11@gmail.com>
2020-11-02 07:09:51 -0500

    added images again

--- js/sketch.js
+++ js/sketch.js
@@ -2,9 +2,36 @@
 
 
 let cart1;
+let avocado;
+let bread;
+let bananas;
+let burritos;
+let chips;
+let eggs;
+let icecream;
+let kombucha;
+let milk;
+let oats;
+let sourworms;
+let strawberries;
+
 
 function preload(){
 cart1= loadImage("images/cart1.png");
+avocado= loadImage("images/avocado.png");
+bread= loadImage("images/bread.png");
+bananas= loadImage("images/bananas.png");
+burritos= loadImage("images/burritos.png");
+chips= loadImage("images/chips.png");
+eggs= loadImage("images/eggs.png");
+icecream= loadImage("images/icecream.png");
+kombucha= loadImage("images/kombucha.png");
+milk= loadImage("images/milk.png");
+oats= loadImage("images/oats.png");
+sourworms= loadImage("images/sourworms.png");
+strawberries= loadImage("images/strawberries.png");
+
+
 }
 function setup() {
 
