ac2820c738e21b55a5aafb8a266f313db1d75dde
emzduck11 <emzduck11@gmail.com>
2020-11-02 06:27:38 -0500

    start

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,5 @@
 
-//let cart1;
-//let sourworms;
-//let strawberries;
 
-
-//function preload(){
-//  cart1= loadImage("images/cart1.png");
-//  sourworms= loadImage("images/sourworms.png");
-  //strawberries= loadImage("images/strawberries.png");
-}
 
 function setup() {
 
